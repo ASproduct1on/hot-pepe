8fe682d16c6fc2e287862d3dee9d1aa86b5500b8
Andrii Skega <Game.a.s.production@gmail.com>
2024-09-05 01:58:57 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import { useMediaQuery } from 'react-responsive'
+import './App.css'
+import eagl from './assets/images/eagl.png'
+import logo from './assets/images/logo.png'
+import pepe2 from './assets/images/pepe2.png'
+import tg from './assets/images/tg.png'
+import x from './assets/images/x.png'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const isDesktop = useMediaQuery({ minWidth: 960 })
+	const isMobile = useMediaQuery({ maxWidth: 959 })
+	return (
+		<div>
+			{isDesktop && (
+				<div className='App'>
+					<div className='logo'>
+						Red <span> TRON </span> Chilli Peppers
+					</div>
+
+					<div className='toolbar-icons'>
+						<div className='icon'>
+							<img src={eagl} alt='Wolf icon' />
+						</div>
+						<div className='icon'>
+							<img src={tg} alt='Paper Plane icon' />
+						</div>
+						<div className='icon'>
+							<img src={x} alt='Scissors icon' />
+						</div>
+					</div>
+
+					<img src={pepe2} alt='Pepe' className='pepe' />
+
+					<div className='content'>
+						<h1 className='title'>
+							The Red <span className='highlight'>TRON</span> Chili Peppers: the
+							#1 Cryptonian rock band
+						</h1>
+						<p className='subtitle'>
+							Fathers of tron rock, tron funk, hip tron and psychodelic tron
+						</p>
+						<div className='buttons'>
+							<button className='button'>sun pump</button>
+							<button className='button button-alt'>CA COPY</button>
+						</div>
+					</div>
+				</div>
+			)}
+			{isMobile && (
+				<div className='App'>
+					<div className='logo'>
+						<img src={logo} alt='Wolf icon' />
+					</div>
+					<main className='main-content'>
+						<div className='text-content'>
+							<div className='toolbar-icons'>
+								<div className='icon'>
+									<img src={eagl} alt='Wolf icon' />
+								</div>
+								<div className='icon'>
+									<img src={tg} alt='Paper Plane icon' />
+								</div>
+								<div className='icon'>
+									<img src={x} alt='Scissors icon' />
+								</div>
+							</div>
+							<h1 className='title'>
+								The Red <span className='highlight'>TRON</span> Chili Peppers:{' '}
+								<br />
+								the #1 Cryptonian rock band
+							</h1>
+							<img src={pepe2} alt='Pepe' className='pepe' />
+						</div>
+
+						<div className='buttons'>
+							<button className='button'>SUN PUMP</button>
+							<button className='button button-alt'>CA COPY</button>
+						</div>
+					</main>
+				</div>
+			)}
+		</div>
+	)
 }
 
-export default App;
+export default App
